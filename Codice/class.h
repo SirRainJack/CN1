4e6ae2508002131420c29bc3e1eddb0abeea2add
devcpp98 <neymanca.98@gmail.com>
2019-03-05 20:37:37 +0100

    Commit class (No Modified)

--- Codice/class.h
+++ Codice/class.h
@@ -7,27 +7,27 @@ using namespace std;
 class Studente {
     
     private:
-    string name;
-    string surname;
-    string id;
+        string name;
+        string surname;
+        string id;
 
     public:
-    Studente(string n, string s, string id){
-        this->name = n;
-        this->surname = s;
-        this->id = id;
-    }
+        Studente(string n, string s, string id){
+            this->name = n;
+            this->surname = s;
+            this->id = id;
+        }
 
-    string getName(){
-        return this->name;
-    }
+        string getName(){
+            return this->name;
+        }
 
-    string getSurname(){
-        return this->surname;
-    }
+        string getSurname(){
+            return this->surname;
+        }
 
-    string getId(){
-        return this->id;
-    }
+        string getId(){
+            return this->id;
+        }
 
 };
