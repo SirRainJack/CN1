5686bf0b4ff948b4f0f00c7b654b7af06f76cb4e
devcpp98 <neymanca.98@gmail.com>
2019-03-05 20:36:26 +0100

    Commit Esercizio3 (Modifed)

--- Codice/Esercizio3.cpp
+++ Codice/Esercizio3.cpp
@@ -1,20 +1,56 @@
-#include <iostream>
+#include "class.h"
 #include <math.h>
 using namespace std;
-int main() {
-    long double dd = 0, cd = 2.0;
-    float df = 0, cf = 2, tmpf;
+
+long double doppiaPrecisione (long double baseDoppia){
+    long double d = 0; //risultato doppia precisione
     double tmpd;
     do {
-        ++df;		
-        tmpf = pow(cf, -df);
-    }while((tmpf + 1) > 1);
-    cout << "Il piu' grande d intero positivo in singola precisione e': "<< df << endl;
-    do {
-        ++dd;		
-        tmpd = pow(cd, -dd);
+        ++d;		
+        tmpd = pow(baseDoppia, -d);
     }while((tmpd + 1) > 1);
-    cout << "Il piu' grande d intero positivo in doppia precisione e': " << dd << endl;
+    return d;
+}
+
+float singolaPrecisione(float baseSingola){
+    float tmpd, d = 0; // risultato singola precisione
+    do {
+        ++d;		
+        tmpd = pow(baseSingola, -d);
+    }while((tmpd + 1) > 1);
+    return d;
+}
+
+int main() {
+    const int N = 2; //numero Studenti
+
+    //dichiarazione degli sviluppatori
+    Studente s[N] = {
+        Studente ("Davide", "Manca", "4475879"),
+        Studente ("Antonio", "Scano", "4520672")
+    };
+    
+    //stampa dati sviluppatori
+    cout << "\n\n=========================\n";
+    cout << "|| PROGETTO A CURA DI: ||\n";
+    cout << "============================================================\n";
+    for(int i = 0; i < N; ++i){
+        cout << "\t" << s[i].getName() << "\t" << s[i].getSurname() << "\t codice di matricola: "<< s[i].getId()<< "\n";
+        cout << "============================================================\n";
+    }
+
+    cout << "\n\n",
+    cout << "=====================================================================================\n";
+    cout << "==================================== ESERCIZIO 3 ====================================\n";
+    cout << "=====================-------------------------------------------=====================\n";
+    cout << "======================= Singola precisione e doppia precisione ======================\n";
+    cout << "=====================================================================================\n";
+    cout << "\n\n";
+
+    long double baseDoppia = 2.0;
+    float baseSingola = 2;
+    cout << "Il piu' grande d intero positivo in singola precisione e': " << singolaPrecisione(baseSingola) << endl;
+    cout << "Il piu' grande d intero positivo in doppia precisione e': " << doppiaPrecisione(baseDoppia) << endl;
     return 0;
 }
 
